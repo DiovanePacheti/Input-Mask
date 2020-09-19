ef68808dddaa7e119068ef5e4492c1b86fdc1a9e
DiovanePacheti <frontendestudo@gmail.com>
2020-09-19 16:59:19 -0300

    implementando a mascara do cep e criando um arquivo mask.ts

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,10 +1,20 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useCallback } from 'react';
 
-const Input: React.FC<InputHTMLAttributes<HTMLInputElement>> = ({...props}) => {
+const Input: React.FC<InputHTMLAttributes<HTMLInputElement>> = ({
+    ...props
+}) => {
+
+    const handleKeyUp = useCallback((e: React.FormEvent<HTMLInputElement>) => {
+        e.currentTarget.maxLength = 9;
+        let value = e.currentTarget.value;
+        value = value.replace(/\D/g, ""); // excluindo tudo que nao for numero e contunie depois de encontrar a primeira ocorrencia e retorna nada
+        value = value.replace(/^(\d{5})(\d)/, "$1-$2"); //pege do inicio 5 digitos e depois o resto dos carateres e etre os dois blocos coloque um -
+        e.currentTarget.value = value;
+    }, []);
 
     return(
         <div>
-            <input {...props}/>
+            <input {...props} onKeyUp={handleKeyUp} />
         </div>
     )
 };
