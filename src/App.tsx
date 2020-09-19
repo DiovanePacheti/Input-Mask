5a3f1e93f300fcb74e2f873f160134f535959105
DiovanePacheti <frontendestudo@gmail.com>
2020-09-19 15:01:33 -0300

    implementando o useState que vai receber uma interface com o estado dos campos do fromulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import Input from './components/Input';
 
+interface ExpressCandidato {
+  cep:string;
+}
+
 const App: React.FC = () => {
 
-  const [candidato, setCandidato] = useState();
+  const [candidato, setCandidato] = useState<ExpressCandidato>({} as ExpressCandidato);
   return (
     <div>
         <Input placeholder="99999-999" />
