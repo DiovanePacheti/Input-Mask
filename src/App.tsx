29d86e6dec26a4d78f8b6e008b2c8bb19b409974
DiovanePacheti <frontendestudo@gmail.com>
2020-09-19 14:17:15 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Input from './components/Input';
+
+const App: React.FC = () => {
+  return (
+    <div>
+        <Input palceholder="99999-999" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Input: React.FC = () => {
+
+    return(
+        <div>
+            <input />
+        </div>
+    )
+};
+
+export default Input;
